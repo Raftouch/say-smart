066acda0fbe5289552447a89e54b87e15241caa1
raftouch <rafanelkin@gmail.com>
2025-05-19 15:26:25 +0200

    update api url for production environment

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:1252",
+  baseURL:
+    import.meta.env.MODE === "production"
+      ? import.meta.env.VITE_API_URL
+      : "http://localhost:1252",
 });
 
 export default api;
