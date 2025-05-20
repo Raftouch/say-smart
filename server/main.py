60bacdf8a120d1ceb701d5de46346aae95570d4e
raftouch <rafanelkin@gmail.com>
2025-05-20 18:48:29 +0200

    set back to microsoft model

--- server/main.py
+++ server/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Query, BackgroundTasks
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from transformers import pipeline
-# from datasets import load_dataset
+from datasets import load_dataset
 from dotenv import load_dotenv
 import soundfile as sf
 import torch
@@ -32,12 +32,9 @@ app.add_middleware(
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# lighter model for production
-synthesiser = pipeline("text-to-speech", "espnet/kan-bayashi_ljspeech_vits", device=device)
-
-# synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts", device=device)
-# embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
-# speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+synthesiser = pipeline("text-to-speech", "microsoft/speecht5_tts", device=device)
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embedding = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0)
 
 
 @app.get("/")
@@ -57,8 +54,7 @@ def say_smart(text: str = Query(..., max_length=200, description="Text to conver
     filename = f"{uuid.uuid4().hex}.wav"
     filepath = os.path.join("output", filename)
 
-    # speech = synthesiser(text, forward_params={"speaker_embeddings": speaker_embedding})
-    speech = synthesiser(text)
+    speech = synthesiser(text, forward_params={"speaker_embeddings": speaker_embedding})
 
     sf.write(filepath, speech["audio"], samplerate=speech["sampling_rate"])
 
